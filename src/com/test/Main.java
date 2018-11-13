667dadfe579cdcbb5f7253eceff5dc5c3e031bb0
zhm <306490609@qq.com>
2018-11-13 22:20:42 +0800

    Add preOrder, inOrder,postOrder,search and find max, find min method
    Add preOrder, inOrder,postOrder,search and find max, find min method

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,19 +1,37 @@
 package com.test;
 
-import com.zhm.BST.BinaryTree;
+import com.zhm.BST.BinarySortTree;
+import com.zhm.BST.Node;
 
 public class Main {
 
     public static void main(String[] args) {
+        BinarySortTree binaryTree = new BinarySortTree();
+        insertIntoBST(binaryTree);
 
-        insertIntoBST();
+        binaryTree.preOrder();
+
+        System.out.println("");
+        binaryTree.inOrder();
+
+        System.out.println("");
+        binaryTree.postOrder();
+
+        System.out.println("");
+        Node result = binaryTree.search(8);
+        System.out.println(result == null ? " Not found" : " " + result.getValue() + " found");
+
+        System.out.println("Maximum Value:" + binaryTree.findMaximumValue());
+
+        System.out.println("Minimum Value:" + binaryTree.findMinimumValue());
+
     }
 
-    private static void insertIntoBST() {
+    private static void insertIntoBST(BinarySortTree binaryTree) {
         int[] nodes = new int[]{8, 3, 10, 1, 6, 14, 4, 7, 13};
-        BinaryTree binaryTree = new BinaryTree();
+        BinarySortTree binarySortTree = new BinarySortTree();
         for (int node : nodes) {
-            binaryTree.insert(node);
+            binarySortTree.insert(node);
         }
     }
 }
---
+++ src/com/zhm/BST/BinarySortTree.java
@@ -0,0 +1,140 @@
+package com.zhm.BST;
+
+/**
+ * Created by zhm on 2018/11/8.
+ */
+public class BinarySortTree {
+    public Node node;
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (node == null) {
+            node = newNode;
+        } else {
+            insertNode(node, newNode);
+        }
+    }
+
+    private void insertNode(Node node, Node newNode) {
+        // Left child
+        if (newNode.getValue() < node.getValue()) {
+            if (node.left == null) {
+                node.left = newNode;
+            } else {
+                insertNode(node.left, newNode);
+            }
+        }
+        // Right child
+        else if (newNode.getValue() > node.getValue()) {
+            if (node.right == null) {
+                node.right = newNode;
+            } else {
+                insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    public void preOrder() {
+        System.out.println("PreOrder:");
+        this.preOrder(node);
+    }
+
+    private void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.getValue() + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void inOrder() {
+        System.out.println("InOrder:");
+        this.inOrder(node);
+    }
+
+    private void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.getValue() + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void postOrder() {
+        System.out.println("InOrder:");
+        this.postOrder(node);
+    }
+
+    private void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.getValue() + " ");
+        }
+    }
+
+    public Node search(int value) {
+        System.out.print("Search:" + value);
+        return search(node, value);
+    }
+
+    private Node search(Node node, int value) {
+        if (node != null) {
+            int currentNodeValue = node.getValue();
+            if (currentNodeValue == value) {
+                return node;
+            } else {
+                if (value < currentNodeValue) {
+                    return search(node.left, value);
+                } else if (value > currentNodeValue) {
+                    return search(node.right, value);
+                }
+            }
+        }
+        return null;
+    }
+
+    public int findMaximumValue() {
+        Node result = findMaximumValue(node);
+
+        if (result == null) {
+            return -1;
+        } else {
+            return result.getValue();
+        }
+    }
+
+    private Node findMaximumValue(Node node) {
+        if (node == null) {
+            return null;
+        } else {
+            while (node.right != null) {
+                node = node.right;
+            }
+            return node;
+        }
+
+    }
+
+    public int findMinimumValue() {
+        Node result = findMinimumValue(node);
+
+        if (result == null) {
+            return -1;
+        } else {
+            return result.getValue();
+        }
+    }
+
+    private Node findMinimumValue(Node node) {
+        if (node == null) {
+            return null;
+        } else {
+            while (node.left != null) {
+                node = node.left;
+            }
+            return node;
+        }
+
+    }
+}
