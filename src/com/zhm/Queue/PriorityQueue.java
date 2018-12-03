c136fb1b512ade7b7e06355d4da9ee872a720609
zhm <306490609@qq.com>
2018-12-03 23:42:18 +0800

    Add PriorityQueue
    Implement PriorityQueue using MaxHeap

---
+++ src/com/zhm/Queue/PriorityQueue.java
@@ -0,0 +1,40 @@
+package com.zhm.Queue;
+
+import com.zhm.Heap.MaxHeap;
+
+/**
+ * Created by zhm on 2018/12/3.
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<E>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+}
