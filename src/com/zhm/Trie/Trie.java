dbd8e363ca6cf11fed3ab83293bb2a8201761deb
zhm <306490609@qq.com>
2018-12-05 17:54:13 +0800

    Add Trie
    implement add method

---
+++ src/com/zhm/Trie/Trie.java
@@ -0,0 +1,53 @@
+package com.zhm.Trie;
+
+import java.util.TreeMap;
+
+/**
+ * Created by zhm on 2018/12/5.
+ */
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    //Add a new word into Trie
+    public void add(String word) {
+        Node cur = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+}
