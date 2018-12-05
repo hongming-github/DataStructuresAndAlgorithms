2eb32186e49b6579a3516d36d4199c0bc65b7d08
zhm <306490609@qq.com>
2018-12-05 23:04:15 +0800

    Add contain and isPrefix methods
    Add contain and isPrefix methods

--- src/com/zhm/Trie/Trie.java
+++ src/com/zhm/Trie/Trie.java
@@ -50,4 +50,31 @@ public class Trie {
         }
     }
 
+    public boolean contains(String word) {
+        Node cur = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return cur.isWord;
+    }
+
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return true;
+    }
 }
