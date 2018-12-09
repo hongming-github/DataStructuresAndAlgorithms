e8c14a6b342aeab98bb71394dc184e5367c8862b
zhm <306490609@qq.com>
2018-12-09 11:34:25 +0800

    Path compression
    Path compression, reduce the depth of the tree

---
+++ src/com/zhm/UnionFind/UnionFind5.java
@@ -0,0 +1,79 @@
+package com.zhm.UnionFind;
+
+/**
+ * Created by zhm on 2018/12/8.
+ */
+public class UnionFind5 implements UF {
+    private int[] parent;
+    //The height of the tree
+    private int[] rank;
+
+    public UnionFind5(int size) {
+        parent = new int[size];
+        rank = new int[size];
+
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    /**
+     * O(h),h is the height of the tree
+     *
+     * @param p
+     * @return
+     */
+    private int find(int p) {
+        if (p < 0 && p >= parent.length)
+            throw new IllegalArgumentException("p is out of bound");
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    /**
+     * O(h)
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * O(h)
+     *
+     * @param p
+     * @param q
+     */
+    @Override
+    public void unionElement(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot)
+            return;
+
+        //将rank低的集合合并到rank高的集合中
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[pRoot] > rank[qRoot]) {
+            parent[qRoot] = pRoot;
+        } else {
+            parent[qRoot] = pRoot;
+            rank[pRoot] += 1;
+        }
+
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+}
